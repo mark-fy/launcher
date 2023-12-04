55ed9c76ebc10fcbb55c9d2645d0182824d9cb1f
BooIMDD <86848614+MarkGG8181@users.noreply.github.com>
2023-12-04 19:42:38 +0200

    add: gradient panel

---
+++ Launcher/GradientPanel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Launcher {
+    class GradientPanel : Panel {
+        public Color TopColor { get; set; }
+        public Color BottomColor { get; set; }
+        protected override void OnPaint(PaintEventArgs e) {
+            LinearGradientBrush gradientBrush = new LinearGradientBrush(this.ClientRectangle, TopColor, BottomColor, 90f);
+            Graphics graphics = e.Graphics;
+            graphics.FillRectangle(gradientBrush, this.ClientRectangle);
+            base.OnPaint(e);
+        }
+    }
+}
